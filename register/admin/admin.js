13bdd551d4bbfe5da7b7d59d072d9ae1ce65c5cf
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2023-12-17 14:57:49 +0500

    delete item done

--- register/admin/admin.js
+++ register/admin/admin.js
@@ -5,10 +5,9 @@ import {
     collection,
     addDoc,
     onSnapshot,
-    setDoc,
     doc,
     query,
-    getDocs
+    deleteDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 import {
@@ -37,6 +36,8 @@ const itemPrice = document.querySelector("#itemPrice");
 const prevItemPrice = document.querySelector("#prevItemPrice");
 
 let imgUrl;
+
+const cardDiv = document.querySelector(".cardDiv");
 
 
 const downloadImageUrl = (file) => {
@@ -143,12 +144,77 @@ addItemBtn.addEventListener("click", async () => {
 const getItems = () => {
     const q = query(collection(db, `restaurants/${adminUid}/menue`));
     onSnapshot(q, (querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            console.log(doc.data());
-            console.log(doc.id);
+        querySnapshot.docChanges().forEach((singleItem) => {
+
+            if (querySnapshot.size) {
+                const yourItems = document.querySelector(".yourItems");
+                yourItems.style.display = "none";
+            }
+
+            if (singleItem.type === "removed") {
+                let dItem = document.getElementById(singleItem.doc.id);
+                if (dItem) {
+                    dItem.remove();
+                }
+            } else if (singleItem.type === "added") {
+                const itemId = singleItem.doc.id;
+                const itemImg = singleItem.doc.data().itemImg;
+                const itemName = singleItem.doc.data().itemName;
+                const itemDesc = singleItem.doc.data().itemDesc;
+                const itemPrice = singleItem.doc.data().itemPrice;
+                const itemType = singleItem.doc.data().itemType;
+                const prevPrice = singleItem.doc.data().prevPrice;
+                let strikePrice;
+                if (prevPrice) {
+                    strikePrice = `
+                    <s><h6>Rs. ${prevPrice}</h6></s>
+                    `
+                }
+                else {
+                    strikePrice = ``;
+                }
+                cardDiv.innerHTML += `
+                <div class="card col-lg-5 col-md-5 col-sm-5 col-12" id="${itemId}">
+                    <div class="cardImgDiv">
+                        <img src="${itemImg}" alt="">
+                    </div>
+                    <div class="cardContentDiv">
+                        <div class="cardNameDiv">
+                            <h5>${itemName}</h5>
+                        </div>
+                        <div class="cardNameDiv">
+                            <h5>${itemDesc}</h5>
+                        </div>
+                        <div class="itemPriceDivOutput">
+                            <h6>Rs. ${itemPrice}</h6>
+
+                            ${strikePrice}
+                        </div>
+                        <div class="itemTypeDiv">
+                            <p><i class="fa-solid fa-circle"></i> </p><h6>${itemType}</h6>
+                        </div>
+
+                        <div class="cardBtnDiv">
+                            <button id="editItemBtn">Edit Item <i class="fa-solid fa-pen-to-square"></i></button>
+                        </div>
+
+                        <div class="cardBtnDiv">
+                            <button id="delBtn" onclick="delBtnFunction('${itemId}')">Delete Item <i class="fa-solid fa-trash"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+            }
         });
     });
 
 }
 
 getItems()
+
+async function delBtnFunction(id) {
+    await deleteDoc(doc(db, `restaurants/${adminUid}/menue`, id));
+}
+
+
+window.delBtnFunction = delBtnFunction;
